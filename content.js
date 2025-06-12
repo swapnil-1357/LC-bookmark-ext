2b07816afb0d3fc704a6fc0c95f8dee8f014a0a0
asur1357 <sukanil1357@gmail.com>
2025-06-12 19:49:53 +0530

    fixing button visibility for spa

--- content.js
+++ content.js
@@ -6,33 +6,99 @@
 // Description: Handles all the webpage level activities (e.g. manipulating page data, etc.)
 // License: MIT
 const bookmarkURL = chrome.runtime.getURL("assets/bookmark.png");
+const AZ_PROBLEM_KEY = "AZ_PROBLEM_KEY"
+// window.addEventListener("load", addBookmarkButton);
 
-window.addEventListener("load", addBookmarkButton);
+const observer = new MutationObserver(() => {
+    addBookmarkButton();
+})
+if(document.body){
+    observer.observe(document.body, { childList: true, subtree: true });
+
+}
+// observer.observe(document.boby,{childList: true, subtree: true });
+addBookmarkButton();
+
+function onProblemsPage() {
+    return window.location.pathname.startsWith('/problems/');
+}
+
+
+
 
 function addBookmarkButton() {
+
+    if (!onProblemsPage() || document.getElementById("add-bookmark-button")) return;
+
     const bookmarkButton = document.createElement('img');
     bookmarkButton.id = "add-bookmark-button";
     bookmarkButton.src = bookmarkURL;
     bookmarkButton.style.height = "30px";
     bookmarkButton.style.width = "30px";
-    bookmarkButton.style.cursor = "pointer";
-    bookmarkButton.style.marginLeft = "auto";
+    // bookmarkButton.style.cursor = "pointer";
+    // bookmarkButton.style.marginLeft = "auto";
 
     const targetDiv = document.getElementsByClassName("coding_nav_bg__HRkIn")[0];
-    const elements = document.getElementsByClassName("items-start")[0];
-    if (elements) {
-        elements.style.display = "flex";
-        elements.appendChild(bookmarkButton);
-    } else {
-        console.warn("Target div not found");
-    }
     if (targetDiv) {
-        targetDiv.style.display = "flex"; 
-        targetDiv.appendChild(bookmarkButton);
-    } else {
-        console.warn("Target div not found");
+        targetDiv.insertAdjacentElement("afterend", bookmarkButton);
+
+    }
+    else {
+        console.warn("error")
     }
 
-    
+    // console.log("hiii")
+    bookmarkButton.addEventListener("click", addNewBookmarkHandler);
+
+
+}
+
+async function addNewBookmarkHandler() {
+    console.log("hi");
+    const currentBookmarks = await getCurrentBookmarks();
+    const azProblemUrl = window.location.href;
+    const uniqueId = extractUniqueId(azProblemUrl);
+    const problemName = document.getElementsByClassName("Header_resource_heading__cpRp1")[0].innerText;
+
+    // this will run on  every bookmark if this will return true then if(ture) and nothing happend means the question is already there;
+    if (currentBookmarks.some((bookmark) => bookmark.id === uniqueId)) return;
+
+    const bookmarkObj = {
+        id: uniqueId,
+        name: problemName,
+        url: azProblemUrl
+
+    }
+
+    const updatedBookmarks = [...currentBookmarks, bookmarkObj];
+
+    chrome.storage.sync.set({ AZ_PROBLEM_KEY: updatedBookmarks }, () => {
+        console.log("updated the bookmarks correctly to ", updatedBookmarks);
+    })
+
+
+}
+
+
+
+function extractUniqueId(url) {
+    const keyword = "problems/";
+    const start = url.indexOf(keyword);
+    // if (start === -1) return null;
+
+    const from = start + keyword.length;
+    const to = url.indexOf("?", from);
+    return to !== -1 ? url.substring(from, to) : url.substring(from);
+}
+
+function getCurrentBookmarks() {
+    return new Promise((resolve, reject) => {
+
+        chrome.storage.sync.get([AZ_PROBLEM_KEY], (results) => {
+            // console.log(results)
+            resolve(results[AZ_PROBLEM_KEY] || []);
+        })
+    })
+
 }
 
